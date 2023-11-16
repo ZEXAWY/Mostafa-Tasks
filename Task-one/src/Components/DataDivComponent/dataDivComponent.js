0337b5902117fbe5c3cdc1ce354ebe45ec91b018
ZEXAWY <freedomblackknights@gmail.com>
2023-11-17 00:40:27 +0200

    Format the Data in table shape

--- Task-one/src/Components/DataDivComponent/dataDivComponent.js
+++ Task-one/src/Components/DataDivComponent/dataDivComponent.js
@@ -17,7 +17,7 @@ function AlbumComponent() {
   return (
     <div className='p-5'>
       <h1 className='text-2xl text-center'>Albums</h1>
-      <ul>
+      {/* <ul>
         {albums.map((album) => (
           <div className='border-black border-2 rounded-md m-2 p-2'>
             <li key={album.userId}>User ID: {album.userId}</li>
@@ -25,7 +25,33 @@ function AlbumComponent() {
             <li key={album.id}>Title: {album.title}</li>
           </div>
         ))}
-      </ul>
+      </ul> */}
+      <table className='min-w-full border-collapse border border-gray-300'>
+        <thead className=''>
+          <tr>
+            <td className='border border-gray-300 px-4 py-2'>UerID</td>
+            <td className='border border-gray-300 px-4 py-2'>ID</td>
+            <td className='border border-gray-300 px-4 py-2 text-center'>
+              Title
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {albums.map((album) => (
+            <tr>
+              <td className='border border-gray-300 px-4 py-2 text-center'>
+                {album.userId}
+              </td>
+              <td className='border border-gray-300 px-4 py-2 text-center'>
+                {album.id}
+              </td>
+              <td className='border border-gray-300 px-4 py-2'>
+                {album.title}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
