01fbf4291509ad5732cd3a2c3fe5864ade577ced
ZEXAWY <freedomblackknights@gmail.com>
2023-11-17 02:32:27 +0200

    adding some basic styling for the table

--- Task-one/src/App.js
+++ Task-one/src/App.js
@@ -2,19 +2,21 @@
 import './App.css';
 import React from 'react';
 import DataDivcomponent from './Components/DataDivComponent/dataDivComponent';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className='h-screen flex flex-col items-center'>
-      <h1 className='text-red-800 text-8xl shadow-lg rounded-lg  pt-10 w-fit text-center'>
-        Task One
-      </h1>
+    <div className=' '>
+      <Header />
+      <div className='h-screen flex flex-col items-center'>
+        <div className='shadow-md mt-10'>
+          <p className='text-3xl font-bold relative shadow-sm'>
+            The First Task
+          </p>
+        </div>
 
-      <h2 className='text-4xl text-black capitalize pt-5 border-black border-b-4 text-center rounded-lg'>
-        this is the first task
-      </h2>
-      <h3 className='text-xl p-2'>Fetching data from another site</h3>
-      <DataDivcomponent />
+        <DataDivcomponent />
+      </div>
     </div>
   );
 }
--- Task-one/src/Components/DataDivComponent/dataDivComponent.js
+++ Task-one/src/Components/DataDivComponent/dataDivComponent.js
@@ -16,7 +16,11 @@ function AlbumComponent() {
 
   return (
     <div className='p-5'>
-      <h1 className='text-2xl text-center m-5'>Albums</h1>
+      <h1 className='text-2xl text-center m-10'>
+        <span className='w-fit bg-gradient-to-t from-gray-300 to-gray-50 p-3 rounded-md'>
+          Albums
+        </span>
+      </h1>
       {/* <ul>
         {albums.map((album) => (
           <div className='border-black border-2 rounded-md m-2 p-2'>
@@ -26,32 +30,34 @@ function AlbumComponent() {
           </div>
         ))}
       </ul> */}
-      <table className='min-w-full border-collapse border border-gray-300'>
-        <thead className=''>
-          <tr>
-            <td className='border border-gray-300 px-4 py-2'>UerID</td>
-            <td className='border border-gray-300 px-4 py-2'>ID</td>
-            <td className='border border-gray-300 px-4 py-2 text-center'>
-              Title
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {albums.map((album) => (
+      <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+        <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+          <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
             <tr>
-              <td className='border border-gray-300 px-4 py-2 text-center'>
-                {album.userId}
-              </td>
-              <td className='border border-gray-300 px-4 py-2 text-center'>
-                {album.id}
-              </td>
-              <td className='border border-gray-300 px-4 py-2'>
-                {album.title}
-              </td>
+              <th className='border border-gray-300 px-6 py-3'>UerID</th>
+              <th className='border border-gray-300 px-6 py-3'>ID</th>
+              <th className='border border-gray-300 px-6 py-3 text-center'>
+                Title
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {albums.map((album) => (
+              <tr className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
+                <td className='border border-gray-300 px-6 py-3  font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                  {album.userId}
+                </td>
+                <td className='border border-gray-300 px-6 py-3 text-center'>
+                  {album.id}
+                </td>
+                <td className='border border-gray-300 px-6 py-3'>
+                  {album.title}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
